6206000c266c8659a4eff9d3c104d4d670721a8d
Wesley Horton <hortowe@ohsu.edu>
2018-10-11 13:40:40 -0700

    Added some functions from utilityFxns.R from my personal repo

--- fxns.R
+++ fxns.R
@@ -121,6 +121,33 @@ subOutput <- function(input, inputId, data, col) {
   return(data)
 } # subOutput
 
+g_legend <- function(a.gplot){
+  #' Extract ggplot legend
+  #' @description Extract legend as separate table from ggplot object
+  #' @param a.gplot a ggplot object with a legend
+  #' @value a gtable object of the legend
+  #' @export
+  
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)
+} # g_legend
 
-
-
+myTableGrob <- function(data_dt, title_v, fontsize_v = 14){
+  #' Create custom table grob with title
+  #' @description Creates table grob in format that is most common for my usage.
+  #' @param data_dt Data.table that the grob will be made out of
+  #' @param title_v Title for display
+  #' @param fontsize_v Fontsize for title. Default is 14 (goes will with my_theme)
+  #' @value gtable object
+  #' @export
+  
+  ## Table
+  table_grob <- tableGrob(data_dt, rows = rep('', nrow(data_dt)), theme = ttheme_minimal())
+  ## Title
+  title_grob <- textGrob(title_v, gp = gpar(fontsize = fontsize_v))
+  ## Add title
+  table_grob <- gtable_add_rows(table_grob, heights = grobHeight(title_grob) + unit(5,'mm'), pos = 0)
+  table_grob <- gtable_add_grob(table_grob, title_grob, 1, 1, 1, ncol(table_grob))
+}
