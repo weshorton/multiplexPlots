96d2b85ead08eccb7417b532a137c65fc3a5178e
Wesley Horton <hortowe@ohsu.edu>
2018-10-16 11:59:50 -0700

    'function' not recognized in .bat. Trying new version

--- run.bat
+++ run.bat
@@ -1,9 +1,3 @@
-### Function to run shiny app
-function runApp() {
-    cmd="R --slave --no-restore -e 'shiny::runApp(\"$1\", launch.browser=T)'"
-    echo $cmd
-    eval $cmd
-}
 
 ### Run
-runApp ./server.R
+R --slave --no-restore -e shiny::runApp('./server.R', launch.browser=T)
