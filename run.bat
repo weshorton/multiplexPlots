31059e3f641e3afc26add2454d64830a1382b9e4
Wesley Horton <hortowe@ohsu.edu>
2018-10-16 12:13:58 -0700

    Third attempt

--- run.bat
+++ run.bat
@@ -1,3 +1,8 @@
-
 ### Run
-R --slave --no-restore -e shiny::runApp('./server.R', launch.browser=T)
+#R --slave --no-restore -e shiny::runApp('./server.R', launch.browser=T)
+
+### Haven't tried this yet - requires a run.r script
+#"C:\Program Files\R\R-3.5.1\bin\Rscript.exe" "C:\Users/wrhor/multiplexPlots/run.R"
+
+###
+"C:\Program Files\R\R-3.5.1\bin\Rscript.exe" -e "shiny::runApp('C:\Users/wrhor/multiplexPlots/run.R', launch.browser = T)"
