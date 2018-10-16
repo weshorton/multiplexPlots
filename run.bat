ed360dcc9fe9c4643c0d053a246711c7f234a87e
Wesley Horton <hortowe@ohsu.edu>
2018-10-16 12:17:33 -0700

    Added sleep command because terminal closes too quickly to read error messages

--- run.bat
+++ run.bat
@@ -5,3 +5,5 @@ REM ### Haven't tried this yet - requires a run.r script
 REM "C:\Program Files\R\R-3.5.1\bin\Rscript.exe" "C:\Users/wrhor/multiplexPlots/run.R"
 
 "C:\Program Files\R\R-3.5.1\bin\Rscript.exe" -e "shiny::runApp('C:\Users/wrhor/multiplexPlots/run.R', launch.browser = T)"
+
+SLEEP 30
