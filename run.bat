87fd655db5631cccde381eb7461e3c8cb8b65a3c
Wesley Horton <hortowe@ohsu.edu>
2018-10-16 12:19:01 -0700

    trying with no other lines

--- run.bat
+++ run.bat
@@ -1,9 +1,2 @@
-REM Run
-REM R --slave --no-restore -e shiny::runApp('./server.R', launch.browser=T)
+"C:\Program Files\R\R-3.5.1\bin\R.exe" -e "shiny::runApp('C:\Users/wrhor/multiplexPlots/run.R', launch.browser = T)"
 
-REM ### Haven't tried this yet - requires a run.r script
-REM "C:\Program Files\R\R-3.5.1\bin\Rscript.exe" "C:\Users/wrhor/multiplexPlots/run.R"
-
-"C:\Program Files\R\R-3.5.1\bin\Rscript.exe" -e "shiny::runApp('C:\Users/wrhor/multiplexPlots/run.R', launch.browser = T)"
-
-SLEEP 30
